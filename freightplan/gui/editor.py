eabfa032b1c0e318abc4302163fe62f0fb2458b4
ZeroKnight <xzeroknightx@gmail.com>
2019-08-06 01:47:00 -0700

    Finally implement proper zoom and panning
    After a few days of studiyng Tiled, Aseprite and GIMP, I've implemented
    the kind of free panning that I was intending, and it finally works with
    the zoom scaling. Feels super great. For almost a week I'd alternate
    between having only one or the other working!

--- freightplan/gui/editor.py
+++ freightplan/gui/editor.py
@@ -18,7 +18,9 @@
 
 import PySide2.QtGui as QtGui
 import PySide2.QtWidgets as QtWidgets
-from PySide2.QtCore import Slot, QMarginsF, QPoint, QPointF, QRectF, Qt
+from PySide2.QtCore import (
+  Signal, Slot, QObject, QPoint, QPointF, QRectF, QSize, Qt
+)
 from PySide2.QtGui import QBrush, QPainter, QPen, QPixmap, QTransform
 from PySide2.QtWidgets import (
   QGraphicsItem, QGraphicsObject, QGraphicsScene, QGraphicsView
@@ -76,6 +78,8 @@ class EditorView(QGraphicsView):
   Editor.
   """
 
+  zoomChanged = Signal(float)
+
   def __init__(self, editor: 'Editor'):
     """Constructor.
 
@@ -85,14 +89,108 @@ class EditorView(QGraphicsView):
 
     super().__init__(editor)
 
-    self.zoomFactor = 1.20
     self._lastMousePos = QPoint()
+    self._scaleFactors = [
+      0.25, 0.33, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5
+    ]
+    self._currentScale = 1
+
+    self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+    self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
 
-  # TODO: Need to manage the sceneRect to always be a certain percent larger
-  # than the grid area, like in Aseprite and Tiled. This way anchored zooming
-  # and more "free" panning is possible.
-  # TODO: Minimum and maximum zoom levels; zoom widget on statusbar
+  def setZoom(self, factor: float):
+    """Set the view's zoom scale to factor."""
+
+    if factor == self._currentScale:
+      return
+    self._currentScale = factor
+    self.zoomChanged.emit(factor)
+
+
+  def zoomIn(self) -> bool:
+    """Zoom the view in to the next increment.
+
+    Returns whether or not the view is able to zoom further.
+    """
+
+    for factor in self._scaleFactors:
+      if factor > self._currentScale:
+        self.setZoom(factor)
+        return True
+    return False
+
+
+  def zoomOut(self) -> bool:
+    """Zoom the view out to the previous increment.
+
+    Returns whether or not the view is able to zoom further.
+    """
+
+    for factor in reversed(self._scaleFactors):
+      if factor < self._currentScale:
+        self.setZoom(factor)
+        return True
+    return False
+
+
+  def resetZoom(self):
+    """Reset the view's zoom scale."""
+
+    self.setZoom(1)
+
+
+  def zoom(self):
+    """Return the view's current zoom scale."""
+
+    return self._currentScale
+
+
+  def resizeEvent(self, event):
+    """Implementation."""
+
+    self.updateSceneRect(self._currentScale)
+
+
+  def updateSceneRect(self, factor: float) -> QRectF:
+    """Updates the associated scene's bounding rectangle.
+
+    To make it possible to pan the view, the scene's bounding rectangle must
+    be larger than the viewport. Scaling the view means that the scene in its
+    entirety is scaled, including the scene's bounding rectangle, so it must
+    be scaled along with the view.
+
+    Currently, the view can be panned such that the editing grid can be
+    freely moved around, but within the bounds of the viewport regardless of
+    its size or zoom level, similar to Aseprite.
+
+    Args:
+      factor: The scaling factor the scene should be sized with respect to.
+
+    Returns the updated scene bounding rectangle.
+    """
+
+    # TODO: make configurable via Property
+    margin = QSize(10, 10) # Between edge of grid and viewport
+    editAreaRect = self.scene().editArea.rect()
+    editAreaSize = editAreaRect.size().toSize()
+
+    # Make the scene 2x larger than the viewport, minus the editing grid. This
+    # allows us to pan the grid up to the edges of the viewport (minus margin),
+    # but no further. Also ensure the scene is at least big enough to encompass
+    # the entire grid.
+    sceneSize = self.maximumViewportSize() * 2 * (1 / factor)
+    sceneSize = sceneSize - editAreaSize - margin
+    sceneSize = sceneSize.expandedTo(editAreaSize + margin)
+    newSceneRect = QRectF(QPointF(0,0), sceneSize)
+    newSceneRect.moveCenter(editAreaRect.center())
+    self.setSceneRect(newSceneRect)
+
+
+  # TODO: Zoom widget on statusbar
+  # TODO: See how much the delta is on horizontal wheel events and add support
+  # for crazy mice like mine.
+  # TODO: handle mice that report deltas finer than 120
   def wheelEvent(self, event: QtGui.QWheelEvent):
     """Implementation.
 
@@ -102,9 +200,16 @@ class EditorView(QGraphicsView):
     if (event.modifiers() & Qt.ControlModifier
         and event.orientation() == Qt.Vertical):
       if event.delta() > 0:
-        self.scale(self.zoomFactor, self.zoomFactor)
+        self.zoomIn()
       else:
-        self.scale(1 / self.zoomFactor, 1 / self.zoomFactor)
+        self.zoomOut()
+
+      # TODO: Improve cursor anchoring; be more like Aseprite
+      prevAnchor = self.transformationAnchor()
+      self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+      self.setTransform(QTransform.fromScale(self.zoom(), self.zoom()))
+      self.updateSceneRect(self.zoom())
+      self.setTransformationAnchor(prevAnchor)
       event.accept()
     else:
       super().wheelEvent(event)
@@ -173,19 +278,20 @@ class Editor(QGraphicsScene):
     self.plan = plan
     self.lastTilePos = None
     self.view = EditorView(self)
+    self.setBackgroundBrush(QBrush(Qt.lightGray))
 
     self.pix = QPixmap(':/images/corridor')
 
     length = cellSize * GRID_SIZE
 
     # Create border for grid area
-    border_rect = QRectF(0, 0, length, length)
+    borderRect = QRectF(0, 0, length, length)
     pen = QPen(Qt.gray)
     brush = QBrush('#5e6787', Qt.SolidPattern)
-    self.setSceneRect(border_rect.marginsAdded(QMarginsF(5, 5, 5, 5)))
 
-    self.editArea = self.addRect(border_rect, pen, brush)
+    self.editArea = self.addRect(borderRect, pen, brush)
     self.grid = EditorGrid(self.editArea)
+    self.view.centerOn(borderRect.center())
 
 
   def sceneToGrid(self, pos: QPointF) -> QPoint:
